42641c1c9f0f9183df742f635bf2d9899ef50159
thederpykrafter <thederpykrafter@gmail.com>
2023-08-13 21:59:48 -0400

    add Tag Providers

--- src/main/java/io/github/thederpykrafter/curiousacquisitions/CuriousAcquisitionsDataGen.java
+++ src/main/java/io/github/thederpykrafter/curiousacquisitions/CuriousAcquisitionsDataGen.java
@@ -2,10 +2,17 @@ package io.github.thederpykrafter.curiousacquisitions;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import io.github.thederpykrafter.curiousacquisitions.datagen.*;
 
 public class CuriousAcquisitionsDataGen implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
---
+++ src/main/java/io/github/thederpykrafter/curiousacquisitions/datagen/ModModelProvider.java
@@ -0,0 +1,25 @@
+package io.github.thederpykrafter.curiousacquisitions.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Models;
+import io.github.thederpykrafter.curiousacquisitions.block.ModBlocks;
+import io.github.thederpykrafter.curiousacquisitions.item.ModItems;
+
+public class ModModelProvider extends FabricModelProvider {
+    public ModModelProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.STRANGE_BLOCK);
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+        itemModelGenerator.register(ModItems.STRANGE_DRINK, Models.GENERATED);
+    }
+}
